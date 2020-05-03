02323fa39f41522bf6c4d8edad1f227ad4c587a0
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-03 22:43:54 +0100

    add attribute remover

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    counter: 0,
+  };
 
+  incrementHandler = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App" data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}{" "}
+        </h1>
+        <button data-test="increment-button" onClick={this.incrementHandler}>
+          Increment counter
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+// import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without errors", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at zero", () => {});
+
+test("clicking button increments counter", () => {});
