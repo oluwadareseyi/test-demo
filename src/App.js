21fba493c3800ee5b94d09cdac743447e42093b6
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-04 03:41:06 +0100

    add test for counter

--- src/App.js
+++ src/App.js
@@ -4,22 +4,44 @@ import "./App.css";
 class App extends Component {
   state = {
     counter: 0,
+    counterError: false,
   };
 
   incrementHandler = () => {
     this.setState((prevState) => ({
+      counterError: false,
       counter: prevState.counter + 1,
     }));
   };
 
+  decrementHandler = () => {
+    if (this.state.counter === 0) {
+      this.setState({ counterError: true });
+      return;
+    }
+    this.setState((prevState) => ({
+      counter: prevState.counter - 1,
+    }));
+  };
+
   render() {
+    const errorMessage = this.state.counterError ? (
+      <h3 data-test="counter-error" className="error">
+        Counter can't go below 0
+      </h3>
+    ) : null;
+
     return (
       <div className="App" data-test="component-app">
         <h1 data-test="counter-display">
-          The counter is currently {this.state.counter}{" "}
+          The counter is currently {this.state.counter}
         </h1>
+        {errorMessage}
         <button data-test="increment-button" onClick={this.incrementHandler}>
           Increment counter
+        </button>
+        <button data-test="decrement-button" onClick={this.decrementHandler}>
+          Decrement counter
         </button>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -6,16 +6,75 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * factory function to create shallow wrapper for the app component
+ * @function setup
+ * @param {object=} props - Component props specific to setup
+ * @param {object=} state - Component state specific to setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  state && wrapper.setState(state);
+  return wrapper;
+};
+
+/**
+ * @function checkElement
+ * @param {string} val - Data attribute which we want to check exits
+ * @returns {null} runs the jest expect function insteaf
+ */
+
+const checkElement = (val) => {
+  const wrapper = setup();
+  const Component = wrapper.find(`[data-test="${val}"]`);
+  expect(Component.length).toBe(1);
+};
+
 test("renders without errors", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
-  expect(appComponent.length).toBe(1);
+  checkElement("component-app");
 });
 
-test("renders increment button", () => {});
+test("renders increment button", () => {
+  checkElement("increment-button");
+});
 
-test("renders counter display", () => {});
+test("renders counter display", () => {
+  checkElement("counter-display");
+});
 
-test("counter starts at zero", () => {});
+test("counter starts at zero", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
 
-test("clicking button increments counter", () => {});
+test("clicking button increments counter", () => {
+  // initialize state in setup to call setState
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  // find the button and similate a click event
+  const button = wrapper.find("[data-test='increment-button']");
+  button.simulate("click");
+
+  // find counter display and test
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
+
+test("count not below zero", () => {
+  // initialize state in setup to cll seState
+  const counter = 0;
+  const wrapper = setup(null, { counter });
+
+  // get the decrement button and simulate a click
+  const button = wrapper.find("[data-test='decrement-button']");
+  button.simulate("click");
+
+  // test if the display contains a value that doesn't go below 0
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const errorDisplay = wrapper.find("[data-test='counter-error']");
+  expect(counterDisplay.text()).toContain(0);
+  expect(errorDisplay.length).toBe(1);
+});
